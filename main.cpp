786557ab51b9fe221a8164d971e9addef9d9a1eb
nonis3 <nonis@purdue.edu>
2018-10-17 11:26:54 -0700

    Ch7 done

--- main.cpp
+++ main.cpp
@@ -6,12 +6,23 @@
 #include "hitable_list.h"
 #include "float.h"
 
+#define rand ((double) rand() / (RAND_MAX))
+
 inline const float MULT = 255.0;
+
+vec3 random_in_unit_sphere() {
+  vec3 p;
+  do {
+    p = 2.0 * vec3(rand, rand, rand) - vec3(1, 1, 1);
+  } while (p.squared_length() >= 1.0);
+  return p;
+}
 
 vec3 color(const ray& r, hitable* world) {
   hit_record rec;
-  if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-    return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+  if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5 * color(ray(rec.p, target - rec.p), world);
   }
   vec3 unit_direction = unit_vector(r.direction());
   float t = 0.5 * (unit_direction.y() + 1.0);
@@ -33,12 +44,13 @@ int main() {
     for (int i = 0; i < nx; i++) {
       vec3 col(0, 0, 0);
       for (int s = 0; s < ns; s++) {
-        float u = float(i + ((double) rand() / (RAND_MAX))) / float(nx);
-        float v = float(j + ((double) rand() / (RAND_MAX))) / float(ny);
+        float u = float(i + rand) / float(nx);
+        float v = float(j + rand) / float(ny);
         ray r = cam.get_ray(u, v);
         col += color(r, world);
       }
       col /= float(ns);
+      col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
       int ir = (MULT * col[0]);
       int ig = (MULT * col[1]);
       int ib = (MULT * col[2]);
